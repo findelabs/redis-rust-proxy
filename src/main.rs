a6290c029b480c8aee357d605185618fae0f8aa7
Dan <circa.1991@gmail.com>
2020-11-15 19:19:06 +0000

    Added sentinel timeout arg

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use tokio::net::TcpListener;
-use clap::{crate_version, App, Arg};
-use net2::TcpBuilder;
-use futures::FutureExt;
-use std::env;
-use std::sync::Arc;
-use std::error::Error;
-use std::net::SocketAddr;
-use tokio::sync::RwLock;
-use env_logger::{Builder, Target};
-use log::LevelFilter;
 use chrono::Local;
-use std::io::Write;
-use std::process::exit;
-use std::iter;
-use rand::{Rng, thread_rng};
+use clap::{crate_version, App, Arg};
+use env_logger::{Builder, Target};
+use futures::FutureExt;
+use log::LevelFilter;
+use net2::TcpBuilder;
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::env;
+use std::error::Error;
+use std::io::Write;
+use std::iter;
+use std::net::SocketAddr;
 use std::net::ToSocketAddrs;
+use std::process::exit;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::sync::RwLock;
 
 mod redis_tools;
 
@@ -23,8 +24,9 @@ mod redis_tools;
 pub struct Inner {
     master: String,
     sentinel_addr: SocketAddr,
+    sentinel_timeout: Duration,
     last_known_master: SocketAddr,
-    discovered_masters: Vec<SocketAddr>
+    discovered_masters: Vec<SocketAddr>,
 }
 
 #[derive(Debug, Clone)]
@@ -34,7 +36,6 @@ pub struct State {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     let opts = App::new("redis-proxy")
         .version(crate_version!())
         .author("Daniel F. <dan@findelabs.com>")
@@ -72,6 +73,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("sentinel_timeout")
+                .short("t")
+                .long("sentinel_timeout")
+                .help("sentinel connection timeout")
+                .required(false)
+                .default_value("10")
+                .env("SENTINEL_TIMEOUT")
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("debug")
                 .short("d")
                 .long("debug")
@@ -96,36 +107,58 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .filter(None, LevelFilter::Info)
         .init();
 
+    // Get listen address and master name
     let listen_addr = opts.value_of("listen").unwrap().to_string();
     let master = opts.value_of("master").unwrap().to_string();
 
-    let sentinel_addr_resolve = opts.value_of("sentinel")
-                        .unwrap()
-                        .to_string()
-                        .to_socket_addrs();
+    // Get sentinel address, convert to socket
+    let sentinel_addr_resolve = opts
+        .value_of("sentinel")
+        .unwrap()
+        .to_string()
+        .to_socket_addrs();
 
+    // Get resolved sentinel address
     let sentinel_addr = match sentinel_addr_resolve {
-        Ok(mut address) => {
-            address.next().unwrap()
-        },
+        Ok(mut address) => address.next().unwrap(),
         Err(e) => {
             log::error!("Error resolving sentinel address: {}", e);
             exit(2)
         }
     };
-            
-//                        .expect("Could not resolve sentinel address provided")
-//                        .next()
-//                        .unwrap();
 
+    // Get sentinel timeout u64
+    let sentinel_timeout_u64 = match opts
+        .value_of("sentinel_timeout")
+        .unwrap()
+        .to_string()
+        .parse::<u64>()
+    {
+        Ok(p) => {
+            log::info!("Using a {}ms sentinel timeout", p);
+            p
+        }
+        Err(e) => {
+            log::error!("Error parsing sentinel_timeout: {}", e);
+            exit(2)
+        }
+    };
+
+    // Get sentinel timeout Duration
+    let sentinel_timeout = Duration::from_millis(sentinel_timeout_u64);
 
     // Get current master, and save into resource
-    let current_master_addr = match redis_tools::get_current_master(&sentinel_addr, &master, &"start") {
-        Ok(socket) => { 
+    let current_master_addr = match redis_tools::get_current_master(
+        &sentinel_addr,
+        &master,
+        &"start",
+        sentinel_timeout,
+    ) {
+        Ok(socket) => {
             log::info!("Current master socket: {}", &socket);
             socket
-        },
-        Err(e) => { 
+        }
+        Err(e) => {
             log::error!("Error connecting to sentinel: {}", e);
             exit(2)
         }
@@ -134,27 +167,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Create initial State
     let state = Inner {
         master: master.clone(),
-        sentinel_addr: sentinel_addr,
+        sentinel_addr,
+        sentinel_timeout,
         last_known_master: current_master_addr,
-        discovered_masters: vec![current_master_addr]
+        discovered_masters: vec![current_master_addr],
     };
 
     // Create shared resource in order to safely pass the current master socket
     let resource = State {
-        inner: Arc::new(RwLock::new(state))
+        inner: Arc::new(RwLock::new(state)),
     };
 
-    let net2_socket = TcpBuilder::new_v4().unwrap()
-        .reuse_address(true).unwrap()
-        .bind(&listen_addr).unwrap()
-        .listen(2048).unwrap();
+    let net2_socket = TcpBuilder::new_v4()
+        .unwrap()
+        .reuse_address(true)
+        .unwrap()
+        .bind(&listen_addr)
+        .unwrap()
+        .listen(2048)
+        .unwrap();
 
     let mut listener = TcpListener::from_std(net2_socket)?;
 
     log::info!("Listening on: {}", listen_addr);
 
     while let Ok((inbound, client)) = listener.accept().await {
-
         let mut rng = thread_rng();
         let id: String = iter::repeat(())
             .map(|()| rng.sample(Alphanumeric))
@@ -164,15 +201,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
         log::info!("{} - New connection from {}", &id, &client);
 
         let transfer = redis_tools::transfer(inbound, resource.clone(), id.clone()).map(move |r| {
-            if let Err(e) = r {
-                log::info!("{} - Connection failed; {}", id, e);
-            }
+            match r {
+                Ok(_) => {
+                    log::info!("{} - Connection completed", id);
+                }
+                Err(e) => {
+                    log::info!("{} - Connection failed; {}", id, e);
+                }
+            };
         });
-    
+
         tokio::spawn(transfer);
     }
 
     Ok(())
 }
-
-
--- src/redis_tools/mod.rs
+++ src/redis_tools/mod.rs
@@ -1,18 +1,20 @@
-use std::net::SocketAddr;
-use redis::{parse_redis_url};
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
-use tokio::io;
 use futures::future::try_join;
+use redis::parse_redis_url;
 use std::error::Error;
+use std::net::SocketAddr;
+use std::time::Duration;
+use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
 
 use crate::State;
 
 // Find out if cache is the master, and return None if any errors are encountered
 pub fn is_master(master_addr: &SocketAddr) -> Result<bool, Box<dyn Error>> {
     let master_connection_format = format!("redis://{}", master_addr);
-    let master_connection_str : &str = &master_connection_format[..];
-    let master_connection_url = parse_redis_url(&master_connection_str).expect("failed to parse redis url");
+    let master_connection_str: &str = &master_connection_format[..];
+    let master_connection_url =
+        parse_redis_url(&master_connection_str).expect("failed to parse redis url");
 
     let client = redis::Client::open(master_connection_url)?;
     let mut con = client.get_connection()?;
@@ -22,26 +24,33 @@ pub fn is_master(master_addr: &SocketAddr) -> Result<bool, Box<dyn Error>> {
 
     match role.as_str() {
         "master" => Ok(true),
-        _ => Ok(false)
+        _ => Ok(false),
     }
 }
 
-pub fn get_current_master(sentinel_addr: &SocketAddr, master: &str, id: &str) -> Result<SocketAddr, Box<dyn Error>> {
-    
+// Get current master from the sentinel
+pub fn get_current_master(
+    sentinel_addr: &SocketAddr,
+    master: &str,
+    id: &str,
+    sentinel_timeout: Duration,
+) -> Result<SocketAddr, Box<dyn Error>> {
     let sentinel_connection_format = format!("redis://{}", sentinel_addr);
-    let sentinel_connection_str : &str = &sentinel_connection_format[..];
+    let sentinel_connection_str: &str = &sentinel_connection_format[..];
     let sentinel_connection_url = parse_redis_url(&sentinel_connection_str).unwrap();
 
     // Establish connection, or return error
     let client = redis::Client::open(sentinel_connection_url)?;
-    let mut con = client.get_connection()?;
+    let mut con = client.get_connection_with_timeout(sentinel_timeout)?;
 
-    let current_master_info : Vec<String> = redis::cmd("SENTINEL")
+    let current_master_info: Vec<String> = redis::cmd("SENTINEL")
         .arg("get-master-addr-by-name")
-        .arg(master).query(&mut con)
+        .arg(master)
+        .query(&mut con)
         .map_err(|e| {
             log::info!("{} - Failed to get current master from sentinel: {}", id, e);
-         }).unwrap();
+        })
+        .unwrap();
 
     let current_master_socket = format!("{}:{}", &current_master_info[0], &current_master_info[1]);
     let current_master_socket = current_master_socket.parse::<SocketAddr>()?;
@@ -49,74 +58,101 @@ pub fn get_current_master(sentinel_addr: &SocketAddr, master: &str, id: &str) ->
     Ok(current_master_socket)
 }
 
+// Look for master from vector of discovered caches
 pub fn find_master(discovered_masters: Vec<SocketAddr>, id: &str) -> Option<SocketAddr> {
+    let masters_pretty: Vec<String> = discovered_masters.iter().map(|v| v.to_string()).collect();
 
-    let masters_pretty: Vec<String> = discovered_masters.iter()
-        .map(|v| v.to_string())
-        .collect();
-
-    log::info!("{} - Searching for the master amongst {}", id, &masters_pretty.join(", "));
+    log::info!(
+        "{} - Searching for the master amongst {}",
+        id,
+        &masters_pretty.join(", ")
+    );
     for master in discovered_masters {
         match is_master(&master) {
-            Ok(true) => { 
+            Ok(true) => {
                 log::info!("{} - Found the master: {}", id, &master);
-                return Some(master)
-            },
+                return Some(master);
+            }
             Ok(false) => {
                 log::info!("{} - It appears that {} is not the master", id, &master);
-                continue
-            },
+                continue;
+            }
             Err(e) => {
                 log::info!("{} - {} error checking for master: {}", id, &master, e);
-                continue
-            },
+                continue;
+            }
         };
     }
     None
 }
 
 //pub async fn transfer<'a>(mut inbound: TcpStream, resource: Arc<RwLock<SocketAddr>>, master: String, sentinel_addr: SocketAddr, id: String) -> Result<(), Box<dyn Error + Send>> {
-pub async fn transfer(mut inbound: TcpStream, resource: State, id: String) -> Result<(), Box<dyn Error + Sync + Send>> {
-
+pub async fn transfer(
+    mut inbound: TcpStream,
+    resource: State,
+    id: String,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
     let resource_read = resource.inner.read().await;
-
     let known_master_socket = resource_read.last_known_master;
 
     // Get current master address, and update resource if socket has changed
-    let current_master_addr = match get_current_master(&resource_read.sentinel_addr, &resource_read.master, &id) {
-        Ok(socket) => socket, 
-        Err(e) => { 
+    let current_master_addr = match get_current_master(
+        &resource_read.sentinel_addr,
+        &resource_read.master,
+        &id,
+        resource_read.sentinel_timeout,
+    ) {
+        Ok(socket) => socket,
+        Err(e) => {
             log::info!("{} - Error getting current master from sentinel: {}", id, e);
-            log::info!("{} - Checking if {} is still the master", id, known_master_socket);
 
             // First, check if the last known master is STILL the master. If it is not,
             // then go through all discovered masters to find the current master
+            log::info!(
+                "{} - Checking if {} is still the master",
+                id,
+                known_master_socket
+            );
             match is_master(&known_master_socket) {
                 Ok(true) => {
-                    log::info!("{} - Looks like {} is still the master", id, &known_master_socket);
+                    log::info!(
+                        "{} - Looks like {} is still the master",
+                        id,
+                        &known_master_socket
+                    );
                     known_master_socket
-                },
+                }
                 _ => {
-                    log::info!("{} - It does not appear that {} is the master", id, &known_master_socket);
+                    log::info!(
+                        "{} - It does not appear that {} is the master",
+                        id,
+                        &known_master_socket
+                    );
                     match find_master(resource_read.discovered_masters.clone(), &id) {
                         Some(s) => s,
                         None => {
-                            log::info!("{} - Could not find current master, defaulting to last master: {}", id, known_master_socket);
+                            log::info!("{} - Could not locate the current master, defaulting to last master: {}", id, known_master_socket);
                             known_master_socket
                         }
                     }
                 }
             }
         }
-    };  
+    };
+
+    // Drop rwlock, to free up more connections
+    drop(resource_read);
 
     // Update current master in State
-    drop(resource_read);
     if current_master_addr != known_master_socket {
         let mut resource_locked = resource.inner.write().await;
         resource_locked.last_known_master = current_master_addr;
         resource_locked.discovered_masters.push(current_master_addr);
-        log::info!("{} - Updated current master address to {}", id, &current_master_addr);
+        log::info!(
+            "{} - Updated current master address to {}",
+            id,
+            &current_master_addr
+        );
     };
 
     let mut outbound = TcpStream::connect(current_master_addr).await?;
@@ -127,15 +163,14 @@ pub async fn transfer(mut inbound: TcpStream, resource: State, id: String) -> Re
     let client_to_server = async {
         io::copy(&mut ri, &mut wo).await?;
         wo.shutdown().await
-    };  
+    };
 
     let server_to_client = async {
         io::copy(&mut ro, &mut wi).await?;
         wi.shutdown().await
-    };  
+    };
 
     try_join(client_to_server, server_to_client).await?;
 
     Ok(())
 }
-
